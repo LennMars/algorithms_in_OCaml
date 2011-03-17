7ff6e2d016e7b8cf45f33d4238ba6a88a9a6f758
LennMars <lennmars@gmail.com>
2011-03-18 02:42:51 +0900

    eratosthenes : more simple

--- eratosthenes/main.ml
+++ eratosthenes/main.ml
@@ -8,20 +8,12 @@ end
 module IntSet = Set.Make(Int)
 
 let eratosthenes n =
-  let remove_multiple base max set =
-    let rec remove_multiple_aux n set =
-    if n > max then set
-    else remove_multiple_aux (n + base) (IntSet.remove n set)
-    in
-    remove_multiple_aux base set
-  in
   let rec eratosthenes_aux set =
     let minimum = IntSet.min_elt set
-    and maximum = IntSet.max_elt set
     in
-    if minimum * minimum > maximum then
+    if minimum * minimum > n then
       set
     else
-      eratosthenes_aux (remove_multiple minimum maximum set)
+      eratosthenes_aux (IntSet.filter (fun m -> m mod minimum <> 0) set)
   in
   List.range 2 n |> List.fold_left (swap_arg IntSet.add) IntSet.empty |> eratosthenes_aux |> IntSet.elements
