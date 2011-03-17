91f80942b7d93bd0761bdf462c9ad93df37c8bc3
LennMars <lennmars@gmail.com>
2011-03-18 03:54:49 +0900

    graph : added undirected graph

--- util/graph.ml
+++ util/graph.ml
@@ -1,24 +1,32 @@
 open Util
 
-module type Anything = sig
+module type Weight = sig
   type t
 end
 
-module type DirectedGraph = sig
+module type Graph = sig
+  (** The type of weights assigned to each edge. *)
   type weight
+
+  (** The type of graphs. *)
   type t
+
+  (** empty n returns a graph with at most n nodes and no edge. *)
   val empty : int -> t
+
+  (** Test whether a graph has no edge or not. *)
   val is_empty : t -> bool
-  val add : int * int -> weight -> t -> unit
-  val set : int * int -> weight -> t -> unit
-  val remove : int * int -> t -> unit
+
+  val add : int * int -> weight -> t -> t
+  val set : int * int -> weight -> t -> t
+  val remove : int * int -> t -> t
   val get : int * int -> t -> weight
   val nodes : t -> int list
 end
 
-module MakeImperativeDirectedGraph (A : Anything) : DirectedGraph with type weight = A.t
+module MakeDirectedGraph (W : Weight) : Graph with type weight = W.t
   = struct
-    type weight = A.t
+    type weight = W.t
     type t = (int * weight) list array
     let is_endpoint e x = fst x = e
     let empty n = Array.make n []
@@ -26,12 +34,16 @@ module MakeImperativeDirectedGraph (A : Anything) : DirectedGraph with type weig
     let exists (s, e) g = List.exists (is_endpoint e) g.(s)
     let add (s, e) weight g =
       if exists (s, e) g then raise (Invalid_argument "edge already exists");
-      g.(s) <- (e, weight) :: g.(s)
+      g.(s) <- (e, weight) :: g.(s);
+      g
     let set (s, e) weight g =
       if exists (s, e) g = false then raise (Invalid_argument "edge doesn't exists");
       g.(s) <- List.remove (is_endpoint e) g.(s);
-      g.(s) <- (e, weight) :: g.(s)
-    let remove (s, e) g = g.(s) <- List.remove (is_endpoint e) g.(s)
+      g.(s) <- (e, weight) :: g.(s);
+      g
+    let remove (s, e) g =
+      g.(s) <- List.remove (is_endpoint e) g.(s);
+      g
     let get (s, e) g = List.find (is_endpoint e) g.(s) |> snd
     let nodes g = Array.mapi (fun i ends -> match ends with [] -> None | _ -> Some i) g
 	     |> Array.to_list
@@ -39,11 +51,38 @@ module MakeImperativeDirectedGraph (A : Anything) : DirectedGraph with type weig
 	     |> List.map (fun x -> match x with Some y -> y | None -> failwith "error")
   end
 
-module Int = struct
-  type t = int
-end
+module MakeUndirectedGraph (W : Weight) : Graph with type weight = W.t
+  = struct
+    type weight = W.t
+    type t = (int * weight) list array
+    let is_endpoint e x = fst x = e
+    let empty n = Array.make n []
+    let is_empty = Array.for_all List.is_empty
+    let exists (s, e) g = List.exists (is_endpoint e) g.(s)
+    let add (s, e) weight g =
+      if exists (s, e) g then raise (Invalid_argument "edge already exists");
+      g.(s) <- (e, weight) :: g.(s);
+      g.(e) <- (s, weight) :: g.(e);
+      g
+    let set (s, e) weight g =
+      if exists (s, e) g = false then raise (Invalid_argument "edge doesn't exists");
+      g.(s) <- List.remove (is_endpoint e) g.(s);
+      g.(s) <- (e, weight) :: g.(s);
+      g.(e) <- List.remove (is_endpoint s) g.(e);
+      g.(e) <- (s, weight) :: g.(e);
+      g
+    let remove (s, e) g =
+      if exists (s, e) g = false then raise (Invalid_argument "edge doesn't exists");
+      g.(s) <- List.remove (is_endpoint e) g.(s);
+      g.(e) <- List.remove (is_endpoint s) g.(e);
+      g
+    let get (s, e) g = List.find (is_endpoint e) g.(s) |> snd
+    let nodes g = Array.mapi (fun i ends -> match ends with [] -> None | _ -> Some i) g
+	     |> Array.to_list
+	     |> List.filter (fun x -> match x with Some _ -> true | None -> false)
+	     |> List.map (fun x -> match x with Some y -> y | None -> failwith "error")
+  end
 
-module IDG = MakeImperativeDirectedGraph(Int)
 
 
 
