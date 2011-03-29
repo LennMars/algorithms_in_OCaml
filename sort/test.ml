7a6002063f80e7a4faee9240619d0b8567281922
LennMars <lennmars@gmail.com>
2011-03-29 19:01:59 +0900

    regrouping.

---
+++ sort/test.ml
@@ -0,0 +1,9 @@
+open Util
+
+let xs = [|4;5;3;2;0;1;6;8;7;5;6;3;1|];;
+
+let _ =
+  Printf.printf "before : ";
+  Array.print string_of_int xs;
+  Printf.printf "after  : ";
+  Array.print string_of_int (Quicksort.quicksort compare xs)
