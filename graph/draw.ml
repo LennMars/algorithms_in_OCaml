610885dc70f92fd43e5f912b368892a69b4a9d17
LennMars <lennmars@gmail.com>
2011-04-10 05:01:13 +0900

    graph : add interface to Ubigraph

--- graph/draw.ml
+++ graph/draw.ml
@@ -3,7 +3,7 @@ open Util
 module Make(G : Graph.Graph) = struct
  let client = new Ubigraph.client "http://localhost:20738/RPC2"
   let u = client#ubigraph;;
-  let draw g =
+  let draw g = (* stub : only nodes yet *)
     u#clear ();
     let vertexes = G.nodes g |>
 	List.map (fun i ->
--- graph/graph.ml
+++ graph/graph.ml
@@ -242,16 +242,5 @@ module UndirectedGraph : Graph = struct
     map_through_aux (List.to_chain path)
 end
 
-let g1 = DirectedGraph.empty 5;;
-DirectedGraph.add (3, 2) () g1;;
-DirectedGraph.add (2, 4) () g1;;
-DirectedGraph.add (3, 4) () g1;;
-
-let g2 = DirectedGraph.empty 5;;
-DirectedGraph.add (0, 1) () g2;;
-DirectedGraph.add (0, 2) () g2;;
-DirectedGraph.add (1, 2) () g2;;
-
-DirectedGraph.unite g1 g2;;
 
 
