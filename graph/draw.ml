db3dd7bfd541c4d808baec848deeb3146e38351e
LennMars <lennmars@gmail.com>
2011-04-10 16:57:17 +0900

    graph : drawer updated.

--- graph/draw.ml
+++ graph/draw.ml
@@ -2,15 +2,26 @@ open Util
 
 module Make(G : Graph.Graph) = struct
  let client = new Ubigraph.client "http://localhost:20738/RPC2"
-  let u = client#ubigraph;;
-  let draw g = (* stub : only nodes yet *)
-    u#clear ();
-    let vertexes = G.nodes g |>
-	List.map (fun i ->
-	  let v = u#new_vertex () in
-	  u#set_vertex_attribute v "label" (string_of_int i);
-	  v
-	)
-    in
-    ()
+ let u = client#ubigraph
+
+ let draw to_string g = (* stub : only nodes yet *)
+   u#clear ();
+   let make_assoc i =
+     let v = u#new_vertex () in
+     u#set_vertex_attribute v "label" (string_of_int i);
+     u#set_vertex_attribute v "fontcolor" "#ff3333";
+     u#set_vertex_attribute v "fontsize" "18";
+     (i, v)
+   in
+   let vertexes = List.map make_assoc (G.nodes g) in
+   let join ((i, j), w) =
+     let e = u#new_edge (List.assoc i vertexes) (List.assoc j vertexes) in
+     u#set_edge_attribute e "label" (to_string w);
+     u#set_edge_attribute e "width" "3.0";
+     u#set_edge_attribute e "color" "#ffffff";
+     u#set_edge_attribute e "fontcolor" "#33cc66";
+     u#set_edge_attribute e "fontsize" "18";
+     ()
+   in
+   List.iter join (G.edges g)
 end
--- graph/test.ml
+++ graph/test.ml
@@ -2,17 +2,16 @@ open Util
 open Graph
 
 let g1 = DirectedGraph.empty 5;;
-DirectedGraph.add (3, 2) () g1;;
-DirectedGraph.add (2, 4) () g1;;
-DirectedGraph.add (3, 4) () g1;;
+DirectedGraph.add (3, 2) "a" g1;;
+DirectedGraph.add (2, 4) "a" g1;;
+DirectedGraph.add (3, 4) "a" g1;;
 
 let g2 = DirectedGraph.empty 5;;
-DirectedGraph.add (0, 1) () g2;;
-DirectedGraph.add (0, 2) () g2;;
-DirectedGraph.add (1, 2) () g2;;
+DirectedGraph.add (0, 1) "b" g2;;
+DirectedGraph.add (0, 2) "b" g2;;
+DirectedGraph.add (1, 2) "b" g2;;
 
 DirectedGraph.unite g1 g2;;
 
-
 module D = Draw.Make(DirectedGraph);;
-D.draw g2
+D.draw identity g2
