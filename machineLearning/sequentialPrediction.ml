025b85f5bba0a11851035f74bdad5e26b44e3acb
LennMars <lennmars@gmail.com>
2011-05-04 22:59:03 +0900

    SequentialPrediction : comment added.

--- machineLearning/sequentialPrediction.ml
+++ machineLearning/sequentialPrediction.ml
@@ -1,37 +1,31 @@
-let ( |> ) f x = x f
+open Util
+
 let anon_fun s = "unknown argument: " ^ s |> print_endline
+(* options init *)
 let to_display = ref false
 let seed = ref 0
 let turn = ref 1
 let prob = ref 0.5
 let m = ref 10
-let spec_display = ("-d", Arg.Bool (fun x -> to_display := x), "");;
-let spec_seed = ("-s", Arg.Int (fun x -> seed := x), "set seed");;
-let spec_turn = ("-t", Arg.Int (fun x -> turn := x), "set turn");;
-let spec_prob = ("-p", Arg.Float (fun x -> prob := x), "set prob");;
+let spec_display = ("-d", Arg.Bool (fun x -> to_display := x), "")
+let spec_seed = ("-s", Arg.Int (fun x -> seed := x), "set seed")
+let spec_turn = ("-t", Arg.Int (fun x -> turn := x), "set turn")
+let spec_prob = ("-p", Arg.Float (fun x -> prob := x), "set prob")
 let spec_m = ("-m", Arg.Int (fun x -> m := x), "set m");;
 Arg.parse [spec_display; spec_seed; spec_turn; spec_prob; spec_m] anon_fun "";;
+Random.init !seed;;
 
-let rec binomial_generator m p ()=
+(* Sampling from binomial distribution whose #experiments is n and success probability is p. *)
+let rec binomial_generator m p () =
   let rec binomial_generator_aux m accum =
-    if m <= 0 then accum else
-      if Random.float 1. < p then binomial_generator_aux (m - 1) (accum + 1) else binomial_generator_aux (m - 1) accum
+    if m <= 0 then
+      accum
+    else
+      binomial_generator_aux (m - 1) (accum + if Random.float 1. < p then 1 else 0)
   in
   binomial_generator_aux m 0;;
 
-Random.init !seed;;
-
-(* let _ = print_int (binomial 5 0.5 ()); print_newline () *)
-
-let identity x = x
-
-let init_list f n =
-  let rec init_list_aux n accum =
-    if n <= 0 then accum else
-      init_list_aux (n - 1) (f (n - 1) :: accum)
-  in
-  init_list_aux n []
-
+(* binomial coefficient C(n, k). *)
 let bin_coef n k =
   if n < k then raise (Invalid_argument "first arg too small");
   let rec fact l accum =
@@ -51,7 +45,7 @@ let binomial_expectator m sequence current_value =
   let max_likelihood_dist x =
     List.fold_left ( *. ) 1. (List.map max_likelihood_dist_point (x :: sequence))
   in
-  let normalizer = List.fold_left (+.) 0. (init_list max_likelihood_dist (m + 1))in
+  let normalizer = List.fold_left (+.) 0. (List.init max_likelihood_dist (m + 1))in
   max_likelihood_dist current_value /. normalizer
 
 let negative_log_loss x =
@@ -66,14 +60,14 @@ let main loss_func generator expectator turn =
       and upper_bound_of_regret = float !m /. 2. *. (log (float turn +. 1.) +. 1.) in
       Printf.printf "regret : %f, upper_bound : %f\n"
 	(total_loss -. min_total_loss) upper_bound_of_regret
-	else
-	  let current_value = generator () in
-	  let loss = loss_func (expectator sequence current_value) in
-	  if !to_display then Printf.printf "turn : %d, value : %d, loss : %f\n" t current_value loss;
-	  main_aux
-	    (total_loss +. loss)
-	    (current_value :: sequence)
-	    (t + 1)
+    else
+      let current_value = generator () in
+      let loss = loss_func (expectator sequence current_value) in
+      if !to_display then Printf.printf "turn : %d, value : %d, loss : %f\n" t current_value loss;
+      main_aux
+	(total_loss +. loss)
+	(current_value :: sequence)
+	(t + 1)
   in
   main_aux 0. [] 1
 
