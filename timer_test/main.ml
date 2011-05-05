b6cf9589c03c78cde505a3269f6992a46541b304
LennMars <lennmars@gmail.com>
2011-05-06 01:29:08 +0900

    timer_test : measuring overhead added.

--- timer_test/main.ml
+++ timer_test/main.ml
@@ -2,13 +2,19 @@ open Unix
 open Util
 
 (** Get resolution of unix timer. *)
-let get_tick () =
+let get_tick n =
   let rec get_tick_aux i accum =
-    if i <= 0 then List.to_chain accum
-      |> List.map (fun (a, b) -> a -. b)
-      |> List.filter ((<) 0.) |> List.find_min identity
+    if i <= 0 then
+      let tick = List.to_chain accum
+	|> List.map (fun (a, b) -> a -. b)
+	|> List.filter ((<) 0.) |> List.find_min identity
+      and overhead = (List.find_max identity accum -. List.find_min identity accum) /. float n
+      in
+      (tick, overhead)
     else get_tick_aux (i - 1) (gettimeofday () :: accum)
   in
-  get_tick_aux 10000 []
+  get_tick_aux n []
 
-let _ = Printf.printf "Unix timer resolution is %.2e sec.\n" (get_tick ());;
+let _ =
+  let (tick, overhead) = get_tick 10000 in
+  Printf.printf "Unix timer : resolution is %.2e sec, overhead is %.2e sec.\n" tick overhead;;
